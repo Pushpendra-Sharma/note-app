bf2711518147fc749261d95331947c88bf669930
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2022-12-29 02:07:53 +0530

    style: updated header styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,13 @@
 import logo from '../assets/logo.png';
 import React from 'react';
+import { useThemeContext } from '../context';
 
 const Nav = () => {
-  let theme = 'light';
+  const { darkTheme, setDarkTheme } = useThemeContext();
 
   const handleTheme = () => {
     console.log('::: to update app theme');
+    setDarkTheme(darkTheme => !darkTheme);
   };
 
   const handleSidebar = () => {
@@ -17,29 +19,43 @@ const Nav = () => {
   };
 
   return (
-    <nav className='flex items-center justify-center p-4 gap-4 border-b-2 shadow-black'>
-      <span className='material-symbols-outlined' onClick={handleSidebar}>
-        menu
-      </span>
-      <img src={logo} alt='Logo' onClick={handleNavigation} />
-      <span className='text-2xl' onClick={handleNavigation}>
-        Keep
-      </span>
-      <input
-        type='search'
-        placeholder='Search'
-        className='rounded-sm px-4 py-2 border-solid border-2 border-black focus:outline-offset-0'
-      ></input>
-      {theme ? (
-        <span className='material-symbols-outlined' onClick={handleTheme}>
-          light_mode
+    <header className='flex items-center justify-between p-2 gap-4 border-b-2 shadow-black'>
+      <div className='flex items-center justify-around pr-8 pl-4'>
+        <span className='material-symbols-outlined px-2' onClick={handleSidebar}>
+          menu
         </span>
-      ) : (
-        <span className='material-symbols-outlined' onClick={handleTheme}>
-          dark_mode
+        <img src={logo} alt='Logo' onClick={handleNavigation} className='px-2'/>
+        <span className='px-2 text-xl' onClick={handleNavigation}>
+          Keep
         </span>
-      )}
-    </nav>
+      </div>
+      <div className='flex items-center justify-around px-4'>
+        <input
+          type='search'
+          placeholder='Search'
+          className='rounded-sm px-4 py-2 border-solid border-2 border-black rounded focus:outline-offset-0'
+        ></input>
+        {/* <span className='material-symbols-outlined'>search</span> */}
+      </div>
+      <div className='flex items-center justify-around px-4'>
+        {darkTheme ? (
+          <span
+            className='material-symbols-outlined cursor-pointer px-4'
+            onClick={handleTheme}
+          >
+            light_mode
+          </span>
+        ) : (
+          <span
+            className='material-symbols-outlined cursor-pointer px-4'
+            onClick={handleTheme}
+          >
+            dark_mode
+          </span>
+        )}
+        <span className='material-symbols-outlined px-4'>apps</span>
+      </div>
+    </header>
   );
 };
 
